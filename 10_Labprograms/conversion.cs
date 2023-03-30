669243cc05383f2e0328ec5be96ae12a9a1b7142
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 17:46:40 +0530

    Add project files.

---
+++ 10_Labprograms/conversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace _10_Labprograms
+{
+    class conversion
+    {
+        static int hex2dec(string hexnum)
+        {
+            int decnum = 0;
+            decnum = int.Parse(hexnum,NumberStyles.HexNumber);
+            return decnum;
+        }
+        static void Main(string[] args)
+        {
+            string hexnum = "";
+            int decnum = 0;
+            Console.WriteLine("enter a hexadecimal number:");
+            hexnum = hex2dec(hexnum);
+            Console.WriteLine("decimal number:" + decnum);
+            Console.ReadLine();
+          
+        }
+
+        
+    }
+}
---
+++ 10_Labprograms/trycatchDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10_Labprograms
+{
+    class trycatchDemo
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("enter first number:");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("enter second number:");
+                int z = x / y;
+                Console.WriteLine("the result of division:" + z);
+
+            }
+            catch(DivideByZeroException)
+            {
+              Console.ForegroundColor=Console.Red;
+
+            }
+        }
+    }
+}
